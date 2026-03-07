689a687245be320d2d727c4696938c498eb378bb
Victor Galaschi <victorgalaschi@hotmail.com>
2026-03-07 09:43:09 -0300

    Aula 1
    * Aula 1
    
    * Aula 1 + Dever de casa: atualizar README
    
    * Aula 1 + Dever de casa: adicionar .gitignore
    
    * Aula 1 + Dever de casa: ajustar descricao do package.json

---
+++ tests/teste.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+// Buscar por um produto e adicionar ao carrinho
+test('Buscar por um produto', async ({ page }) => {
+
+    // Acessar a página inicial
+    await page.goto('https://hatstore-prd.fly.dev/');
+
+    // Buscar por um produto
+    await page.fill('input[placeholder="Buscar chapéu..."]', 'Chapéu Floppy');
+
+    // Adicionar item ao carrinho
+    await page.click('button:has-text("Adicionar ao carrinho")');
+
+    // Validar que o item foi adicionado ao carrinho
+    await expect(page.locator('.cart-item-nome')).toHaveText('Chapéu Floppy');
+});
